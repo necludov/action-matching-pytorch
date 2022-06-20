256c66581cbcc1fed594f47d33d47cc72aa82bd5
kirill <kirill@q.vector.local>
2022-06-20 12:22:09 -0400

    dataset normalizations

--- config_cifar10_32.py
+++ config_cifar10_32.py
@@ -6,8 +6,8 @@ def get_configs():
     model_dict.nf = 128
     model_dict.ch_mult = (1, 2, 2, 2)
     model_dict.num_res_blocks = 2
-    model_dict.attn_resolutions = (16,)
-    model_dict.dropout = 0.1
+    model_dict.attn_resolutions = (16, 8)
+    model_dict.dropout = 0.0
     model_dict.resamp_with_conv = True
     model_dict.conditional = True
     model_dict.nonlinearity = 'swish'
@@ -22,8 +22,8 @@ def get_configs():
     data_dict.num_channels = 3
     data_dict.centered = True
     data_dict.batch_size = 128
-    data_dict.norm_mean = (0.5, 0.5, 0.5)
-    data_dict.norm_std = (0.5, 0.5, 0.5)
+    data_dict.norm_mean = (0.4914, 0.4822, 0.4465)
+    data_dict.norm_std = (0.2470, 0.2435, 0.2616)
     train_dict = dotdict()
     train_dict.grad_clip = 1.0
     train_dict.warmup = 0
--- config_mnist.py
+++ config_mnist.py
@@ -7,11 +7,11 @@ def get_configs():
     model_dict.ch_mult = (1, 2, 2)
     model_dict.num_res_blocks = 2
     model_dict.attn_resolutions = (16,)
-    model_dict.dropout = 0.1
+    model_dict.dropout = 0.0
     model_dict.resamp_with_conv = True
     model_dict.conditional = True
     model_dict.nonlinearity = 'swish'
-    model_dict.savepath = 'am_mnist_vpsde'
+    model_dict.savepath = 'am_mnist_vpsde_dropout0_normed'
     model_dict.s = 'generic'
     model_dict.w = w1
     model_dict.dwdt = dw1dt
@@ -22,8 +22,8 @@ def get_configs():
     data_dict.num_channels = 1
     data_dict.centered = True
     data_dict.batch_size = 128
-    data_dict.norm_mean = (0.5)
-    data_dict.norm_std = (0.5)
+    data_dict.norm_mean = (0.1309)
+    data_dict.norm_std = (0.2893)
     train_dict = dotdict()
     train_dict.grad_clip = 1.0
     train_dict.warmup = 0
